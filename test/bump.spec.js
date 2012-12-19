6acc13487792d2f1985fbd375d5145516ea79449
adam j. sontag <ajpiano@ajpiano.com>
2012-12-18 19:12:02 -0500

    Support grunt 0.4.x by moving bump_version out of a helper, using grunt-contrib-nodeunit

--- test/bump.spec.js
+++ test/bump.spec.js
@@ -1,11 +1,9 @@
-var grunt = require('grunt');
-
-require('../tasks/bump')(grunt);
+var bumpVersion = require("../tasks/bump/index.js");
 
 exports['bump_version'] = function(test) {
   test.expect(3);
-  test.equal(grunt.helper('bump_version', '0.0.1', 'patch'), '0.0.2');
-  test.equal(grunt.helper('bump_version', '1.0.1', 'minor'), '1.1.0');
-  test.equal(grunt.helper('bump_version', '1.5.1', 'major'), '2.0.0');
+  test.equal(bumpVersion('0.0.1', 'patch'), '0.0.2');
+  test.equal(bumpVersion('1.0.1', 'minor'), '1.1.0');
+  test.equal(bumpVersion('1.5.1', 'major'), '2.0.0');
   test.done();
 };
