59d625ed29be23646cf882313f8fa241eb6689eb
James Costian <james@jamescostian.com>
2013-04-20 10:27:28 -0500

    Got rid of tests (they're useless now)

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,18 +2,8 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    nodeunit: {
-      bump: ['test/*.spec.js']
-    }
   });
 
   // Load local tasks.
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
-  // Load local tasks.
   grunt.loadTasks('tasks');
-
-  // Default task.
-  grunt.registerTask('default', 'nodeunit');
-  grunt.registerTask('test', 'nodeunit');
 };
--- test/bump.spec.js
+++
@@ -1,9 +0,0 @@
-var bumpVersion = require("../tasks/bump/index.js");
-
-exports['bump_version'] = function(test) {
-  test.expect(3);
-  test.equal(bumpVersion('0.0.1', 'patch'), '0.0.2');
-  test.equal(bumpVersion('1.0.1', 'minor'), '1.1.0');
-  test.equal(bumpVersion('1.5.1', 'major'), '2.0.0');
-  test.done();
-};
