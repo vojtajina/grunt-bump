1c73ba519594e970e2154cebd3d3a997ea7e7a7d
James Costian <james@jamescostian.com>
2013-04-26 17:22:16 -0500

    Rewrote Gruntfile.js in my own style

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,4 @@
-module.exports = function(grunt) {
-
-  // Project configuration.
-  grunt.initConfig({
-  });
-
-  // Load local tasks.
-  grunt.loadTasks('tasks');
-};
+module.exports = function (grunt) {
+    grunt.initConfig({})
+    grunt.loadTasks('tasks')
+}
