5a93c7371dcd0fae9786f06aece3f350a3075a24
Vojta Jina <vojta.jina@gmail.com>
2013-07-05 16:47:53 -0700

    chore: add auto release task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,16 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({});
+  grunt.initConfig({
+    'auto-release': {
+      options: {
+        checkTravisBuild: false
+      }
+    }
+  });
+
   grunt.loadTasks('tasks');
   grunt.loadNpmTasks('grunt-npm');
+  grunt.loadNpmTasks('grunt-auto-release');
 
   grunt.registerTask('release', 'Bump version, push to NPM.', function(type) {
     grunt.task.run([
