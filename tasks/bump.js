0fab670d0583527c6c32e2205f6c24a4a0244aed
Freddy Knuth <freddy.knuth@rackspace.com>
2015-08-26 14:16:42 -0500

    feat: allow git commit options
    Closes #156

--- tasks/bump.js
+++ tasks/bump.js
@@ -15,6 +15,7 @@ module.exports = function(grunt) {
       createTag: true,
       dryRun: false,
       files: ['package.json'],
+      gitCommitOptions: '',
       gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
       globalReplace: false,
       prereleaseName: false,
@@ -172,7 +173,7 @@ module.exports = function(grunt) {
       var commitMessage = opts.commitMessage.replace(
         '%VERSION%', globalVersion
       );
-      var cmd = 'git commit ' + opts.commitFiles.join(' ');
+      var cmd = 'git commit ' + opts.gitCommitOptions + ' ' + opts.commitFiles.join(' ');
       cmd += ' -m "' + commitMessage + '"';
 
       if (dryRun) {
